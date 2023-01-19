5944bd7f5020e98916d17c5af51958c399210122
Metarails <SamiSantamaa87@gmail.com>
2023-01-19 11:33:51 +0200

    fic to strogaes_id and moving Storage table

--- products/models.py
+++ products/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from storages.models import Storage
-
 
 # Create your models here.
 class Picture(models.Model):
@@ -9,6 +7,12 @@ class Picture(models.Model):
     id = models.BigAutoField(primary_key=True)
     picture_address = models.ImageField(upload_to="pictures")
 
+class Storage(models.Model):
+    """class for making Storage table for database"""
+
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
 
 class Product(models.Model):
     """class for making Product table for database"""
@@ -19,9 +23,10 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     estimate_price = models.FloatField(null=True, blank=True)
     price = models.FloatField(default=0.0)
-    storages_id = models.ForeignKey(Storage, on_delete=models.DO_NOTHING)  # from app users
+    storages_id = models.ForeignKey(Storage, on_delete=models.SET_NULL, null=True, blank=True)  # from app users
     shelf_id = models.IntegerField(blank=True, null=True)
     free_description = models.TextField(blank=True, null=True)
     pictures = models.ManyToManyField(Picture)
     condition = models.CharField(max_length=50, blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
+

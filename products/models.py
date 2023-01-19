8bc85ecb134facedaa987b6f7b7022c356fb5b68
Metarails <SamiSantamaa87@gmail.com>
2023-01-19 15:02:26 +0200

    kenttien lisäyksiä/muutoksia

--- products/models.py
+++ products/models.py
@@ -13,21 +13,21 @@ class Storage(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
+    in_use = models.BooleanField(default=True)
 
 class Product(models.Model):
     """class for making Product table for database"""
     id = models.BigAutoField(primary_key=True)
     available = models.BooleanField(default=False)
     barcode = models.CharField(max_length=255, blank=True, null=True)
-    category = models.CharField(max_length=255)
-    group_id = models.IntegerField(blank=True,null=True)
+    category = models.CharField(max_length=255)  # remember to link to categories after categories_table is created
+    group_id = models.CharField(max_length=255, blank=True, null=True)
     name = models.CharField(max_length=255)
     estimate_price = models.FloatField(null=True, blank=True)
     price = models.FloatField(default=0.0)
-    storages_id = models.ForeignKey(Storage, on_delete=models.SET_NULL, null=True, blank=True)  # from app users
+    storages_id = models.ForeignKey(Storage, on_delete=models.SET_NULL, null=True, blank=True)
     shelf_id = models.IntegerField(blank=True, null=True)
     free_description = models.TextField(blank=True, null=True)
     pictures = models.ManyToManyField(Picture)
     condition = models.CharField(max_length=50, blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
-

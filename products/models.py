cc73d0e4f65ce83f6a3e5c91605bee95f1572356
Metarails <SamiSantamaa87@gmail.com>
2023-01-24 08:09:52 +0200

    added/changed fields with new info, migrations

--- products/models.py
+++ products/models.py
@@ -22,11 +22,11 @@ class Product(models.Model):
     id = models.BigAutoField(primary_key=True)
     available = models.BooleanField(default=False)
     barcode = models.CharField(max_length=255, blank=True, null=True)
-    # remember to link to categories after categories_table is created
+    # Linked to category table
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     group_id = models.CharField(max_length=255, blank=True, null=True)
     name = models.CharField(max_length=255)
-    estimate_price = models.FloatField(null=True, blank=True)
+    #estimate_price = models.FloatField(null=True, blank=True) #not needed
     price = models.FloatField(default=0.0)
     #table startages_id Id link field
     storages = models.ForeignKey(Storage, on_delete=models.SET_NULL, null=True, blank=True)
@@ -34,5 +34,8 @@ class Product(models.Model):
     free_description = models.TextField(blank=True, null=True)
     #linked to pictures table
     pictures = models.ManyToManyField(Picture)
-    condition = models.CharField(max_length=50, blank=True, null=True)
+    #condition = models.CharField(max_length=50, blank=True, null=True) #condision is explained in  desc field
     date = models.DateTimeField(auto_now_add=True)
+    measurements = models.CharField(max_length=50, null=True, blank=True)
+    color = models.CharField(max_length=50, null=True, blank=True)
+    weight = models.FloatField(default=0.0, blank=True, null=True)

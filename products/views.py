bc3807b38d65bde8eff5f5b7b7727b7937bed484
Minkkinen Juho <juho.minkkinen@turku.fi>
2023-02-01 14:18:22 +0200

    Added pagination for products list and category products list, general changes to classes in views

--- products/views.py
+++ products/views.py
@@ -1,32 +1,39 @@
 from rest_framework.decorators import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from django.core.exceptions import ObjectDoesNotExist
 from .models import Product
 from .serializers import ProductSerializer
 
 # Create your views here.
+class ProductListPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = "page_size"
 
 
-class ProductList(APIView):
-    def get(self, request, format=None):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+class CategoryProductListPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = "page_size"
 
-    def post(self, request, format=None):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = ProductListPagination
+
+
+class CategoryProductList(generics.ListAPIView):
+    serializer_class = ProductSerializer
+    pagination_class = CategoryProductListPagination
+
     def get_queryset(self):
-        category = self.kwargs['category']
-        return Product.objects.filter(category_name=category)
+        category = self.kwargs["category_id"]
+        return Product.objects.filter(category=category)
 
 
-class ProductCategory(APIView):
+class ProductDetail(APIView):
     def get_object(self, pk):
         try:
             return Product.objects.get(pk=pk)

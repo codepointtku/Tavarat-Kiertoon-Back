1e2b9d1b8747ead51e027a076f0ac849fff696b6
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-02-02 10:13:31 +0200

    Update views.py

--- products/views.py
+++ products/views.py
@@ -1,20 +1,16 @@
-from rest_framework.decorators import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination
-from django.core.exceptions import ObjectDoesNotExist
 from .models import Product
 from .serializers import ProductSerializer
 
 # Create your views here.
 class ProductListPagination(PageNumberPagination):
-    page_size = 100
+    page_size = 2
     page_size_query_param = "page_size"
 
 
 class CategoryProductListPagination(PageNumberPagination):
-    page_size = 100
+    page_size = 1
     page_size_query_param = "page_size"
 
 
@@ -33,27 +29,6 @@ class CategoryProductList(generics.ListAPIView):
         return Product.objects.filter(category=category)
 
 
-class ProductDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Product.objects.get(pk=pk)
-        except ObjectDoesNotExist:
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def get(self, request, pk, format=None):
-        products = self.get_object(pk)
-        serializer = ProductSerializer(products)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        products = self.get_object(pk)
-        serializer = ProductSerializer(products, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        products = self.get_object(pk)
-        products.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer

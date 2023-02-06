0341b9ee8d0f96f78d899232bde37e691835cc05
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-02-06 11:15:19 +0200

    Equalized views naming to format agreed upon

--- products/views.py
+++ products/views.py
@@ -16,7 +16,7 @@ class CategoryProductListPagination(PageNumberPagination):
     page_size_query_param = "page_size"
 
 
-class ProductList(generics.ListCreateAPIView):
+class ProductListView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = ProductListPagination
@@ -25,7 +25,7 @@ class ProductList(generics.ListCreateAPIView):
     ordering = ["date", "name", "color"]
 
 
-class CategoryProductList(generics.ListAPIView):
+class CategoryProductListView(generics.ListAPIView):
     serializer_class = ProductSerializer
     pagination_class = CategoryProductListPagination
     filter_backends = [OrderingFilter]
@@ -41,37 +41,37 @@ class CategoryProductList(generics.ListAPIView):
         return Product.objects.filter(category__in=categoryset)
 
 
-class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 
-class ColorList(generics.ListCreateAPIView):
+class ColorListView(generics.ListCreateAPIView):
     queryset = Color.objects.all()
     serializer_class = ColorSerializer
 
 
-class ColorDetail(generics.RetrieveUpdateDestroyAPIView):
+class ColorDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Color.objects.all()
     serializer_class = ColorSerializer
 
 
-class StorageList(generics.ListCreateAPIView):
+class StorageListView(generics.ListCreateAPIView):
     queryset = Storage.objects.all()
     serializer_class = StorageSerializer
 
 
-class StorageDetail(generics.RetrieveUpdateDestroyAPIView):
+class StorageDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Storage.objects.all()
     serializer_class = StorageSerializer
 
 
-class PictureList(generics.ListCreateAPIView):
+class PictureListView(generics.ListCreateAPIView):
     queryset = Picture.objects.all()
     serializer_class = PictureSerializer
 
 
-class PictureDetail(generics.RetrieveUpdateDestroyAPIView):
+class PictureDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Picture.objects.all()
     serializer_class = PictureSerializer

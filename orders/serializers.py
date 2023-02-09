4b906cdceb8c29dc899c738ea1bb96be80c4e254
Metarails <SamiSantamaa87@gmail.com>
2023-02-09 08:30:29 +0200

    removed own testing stuff from different model

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Order, Product, ShoppingCart
+from .models import Order, ShoppingCart
 
 
 class ShoppingCartSerializer(serializers.ModelSerializer):
@@ -8,20 +8,8 @@ class ShoppingCartSerializer(serializers.ModelSerializer):
         model = ShoppingCart
         fields = "__all__"
 
-class testSer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = "__all__"
-        #fields = "id", "price"
-
-
 class OrderSerializer(serializers.ModelSerializer):
     
-    #products = testSer(read_only=True, many=True) #MIKSI EI TOIMIIIIIIIIS
-    #test2 = serializers.CharField(default="test2")
-    #products = serializers.SlugRelatedField(many=True, read_only=True, slug_field='name')
-
     class Meta:
         model = Order
         fields = "__all__"
-        #depth = 1

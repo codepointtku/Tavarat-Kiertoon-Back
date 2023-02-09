8ba19e822da78a6f5016fc55489092ba98ddbfca
Platos-Man <joonasru@tutanota.com>
2023-02-09 12:25:07 +0200

    changed database to postgresql

--- tavarat_kiertoon/settings.py
+++ tavarat_kiertoon/settings.py
@@ -89,10 +89,20 @@ WSGI_APPLICATION = "tavarat_kiertoon.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "tavarat_kiertoon",
+        "USER": "postgres",
+        "PASSWORD": "testi123",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 

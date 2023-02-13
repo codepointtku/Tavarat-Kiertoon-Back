4de85044f9e643d557e367f8e1f1aedbb4840594
Kasper Kerajärvi <kasperkerajarvi@gmail.com>
2023-02-13 15:24:04 +0200

    Added a test view

--- bikes/views.py
+++ bikes/views.py
@@ -1,8 +1,25 @@
+import datetime
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from bikes.serializers import BikeSerializer
+# TODO: Write docstrings
 
-from .models import Bike
 
-# Create your views here.
+@api_view(["GET"])
+def test(request):
+    today = datetime.date.today()
+    available_from = today + datetime.timedelta(days=7)
+    available_to = today + datetime.timedelta(days=183)
+    closest_monday = today - datetime.timedelta(days=today.weekday())
+    return Response(
+        {
+            "date_info": {
+                "today": today,
+                "available_from": available_from,
+                "available_to": available_to,
+                "closest_monday": closest_monday,
+            },
+            "bikes": {},
+        }
+    )

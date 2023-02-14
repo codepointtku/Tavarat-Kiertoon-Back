f7a9dd679f81bf6b59b3ea9b532eca3af7fde6ff
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-02-14 14:59:23 +0200

    replaced categories views

--- categories/views.py
+++ categories/views.py
@@ -1,41 +1,14 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import generics, status, response
+
 from .models import Category
 from .serializers import CategorySerializer
+
 # Create your views here.
-@api_view(['GET', 'POST'])
-def categories(request):
+class CategoryListView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-    if request.method == 'GET':
-        data = Category.objects.all()
-        serializer = CategorySerializer(data, many=True)
-        return Response({"categories": serializer.data})
 
-    elif request.method == 'POST':
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def category(request, category_id):
-
-    try:
-        item = Category.objects.get(pk=category_id)
-    except ObjectDoesNotExist:
-        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    if request.method == 'GET':
-        serializer = CategorySerializer(item)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = CategorySerializer(item, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
-    elif request.method == 'DELETE':
-        item.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer

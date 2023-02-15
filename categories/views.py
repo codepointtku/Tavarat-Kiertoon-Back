ebb39ace51a925cd426f4c9ea86bc6bf60b68ea9
Minkkinen Juho <juho.minkkinen@turku.fi>
2023-02-15 12:21:54 +0200

    Added check for category level.
    Added check for category level when adding a new category. If the category level is over set threshold, returns bad request.

--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,5 @@
-from rest_framework import generics, status, response
-
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -8,6 +8,15 @@ class CategoryListView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    def post(self, request, *args, **kwargs):
+        instance = request.data
+        levelchecker = Category.objects.filter(id=instance["parent"]).values("level")
+        level = list(levelchecker)
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid()and level[0]["level"] < 2:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()

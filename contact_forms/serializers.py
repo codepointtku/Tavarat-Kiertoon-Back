a7ac90f789097f3d692f27cab97410e861e78502
GitPalin <30406273+GitPalin@users.noreply.github.com>
2023-02-16 14:52:10 +0200

    fix ugly camelCase to clearly superior snake_case

--- contact_forms/serializers.py
+++ contact_forms/serializers.py
@@ -10,9 +10,7 @@ class ContactFormSerializer(serializers.ModelSerializer):
 
 
 class ContactSerializer(serializers.ModelSerializer):
-    # phone_number in the form that front wants
-    phoneNumber = serializers.ReadOnlyField(source="phone_number")
 
     class Meta:
         model = Contact
-        fields = ["name", "address", "email", "phoneNumber"]
+        fields = "__all__"

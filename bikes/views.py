baf93ed0429bbfa289db68eeb0916d7f964a0956
Kasper Kerajärvi <kasperkerajarvi@gmail.com>
2023-02-20 16:10:33 +0200

    Cleaned code

--- bikes/views.py
+++ bikes/views.py
@@ -8,11 +8,12 @@ from rest_framework.response import Response
 
 @api_view(["GET"])
 def test(request):
-    """Just a test function I'll remove later"""
+    """Just a test function I'll remove later."""
+
     today = datetime.date.today()
     available_from = today + datetime.timedelta(days=7)
     available_to = today + datetime.timedelta(days=183)
-    monday = today - datetime.timedelta(days=today.weekday() + 7)
+
     return Response(
         {
             "date_info": {

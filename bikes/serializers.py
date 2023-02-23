456fbe57c64e0605d893425606c94676e971ac19
Kasper Kerajärvi <kasperkerajarvi@gmail.com>
2023-02-23 09:15:18 +0200

    Progress on returning real data

--- bikes/serializers.py
+++ bikes/serializers.py
@@ -4,6 +4,11 @@ from .models import Bike
 
 
 class BikeSerializer(serializers.ModelSerializer):
+    type_name = serializers.ReadOnlyField(source="type.name")
+    brand_name = serializers.ReadOnlyField(source="brand.name")
+    size_name = serializers.ReadOnlyField(source="size.name")
+    color_name = serializers.ReadOnlyField(source="color.name")
+
     class Meta:
         model = Bike
         fields = "__all__"
--- bikes/views.py
+++ bikes/views.py
@@ -5,6 +5,8 @@ import datetime
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from bikes.models import Bike
+
 
 @api_view(["GET"])
 def test(request):
@@ -14,18 +16,20 @@ def test(request):
     available_from = today + datetime.timedelta(days=7)
     available_to = today + datetime.timedelta(days=183)
 
+    queryset = Bike.objects.all().values()
+
     return Response(
         {
             "date_info": {
                 "available_from": available_from,
                 "available_to": available_to,
             },
+            "bikey_boo": queryset,
             "bikes": [
                 {
                     "id": 1,
                     "name": "Punainen hieno pyörä",
                     "description": "Hyväkuntone hieno pyörä punainen suoraa 80-luvulta",
-                    "available": 0,
                     "max_available": 0,
                     "taken": {},
                     "size": "14″",
@@ -37,7 +41,6 @@ def test(request):
                     "id": 2,
                     "name": "Vihreä hyvä pyörä",
                     "description": "Todella hyvä pyörä",
-                    "available": 5,
                     "max_available": 17,
                     "taken": {"15.3.2023": 1, "16.3.2023": 2, "20.3.2023": 1},
                     "size": "21″",
@@ -49,7 +52,6 @@ def test(request):
                     "id": 3,
                     "name": "Toinen hyvä pyörä",
                     "description": "Todella hyvä pyörä myös",
-                    "available": 2,
                     "max_available": 9,
                     "taken": {"15.3.2023": 1, "16.3.2023": 2, "20.3.2023": 1},
                     "size": "16″",
@@ -61,7 +63,6 @@ def test(request):
                     "id": 4,
                     "name": "Päiväkoti -paketti",
                     "description": "16″ pyöriä 7 kpl, 14″ pyöriä 3 kpl, potkupyöriä 10 kpl, pyöräilykypäriä 20 kpl, käsipumppu, jalkapumppu, monitoimityökalu",
-                    "available": 1,
                     "max_available": 2,
                     "taken": {"15.3.2023": 1, "16.3.2023": 2, "20.3.2023": 1},
                     "size": "14″ & 16″",
@@ -73,7 +74,6 @@ def test(request):
                     "id": 5,
                     "name": "Koulu -paketti",
                     "description": "20″ pyöriä 6 kpl, 24″ pyöriä 6 kpl, pyöräilykypäriä 13 kpl, käsipumppu, jalkapumppu, monitoimityökalu, molempia pyöriä olemassa 7 kpl, mutta tällä määrällä peräkärry on helppo lastata",
-                    "available": 1,
                     "max_available": 2,
                     "taken": {"15.3.2023": 1, "16.3.2023": 2, "20.3.2023": 1},
                     "size": "20″ & 24″",

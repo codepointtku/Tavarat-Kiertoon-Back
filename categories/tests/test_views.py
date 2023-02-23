91da9e4920fec6c56773c8695d987c66f8bf1924
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-02-23 13:39:01 +0200

    temporary
    temp testing

--- categories/tests/test_views.py
+++ categories/tests/test_views.py
@@ -6,11 +6,16 @@ from categories.models import Category
 
 class TestCategories(TestCase):
     def setUp(self):
-        self.test_category = Category.objects.create(name="coffee", parent=None)
+        self.test_category = Category.objects.create(name="coffee")
         self.test_category0 = Category.objects.create(
             name="pot", parent=self.test_category
         )
-
+        self.test_category1 = Category.objects.create(
+            name="bean", parent=self.test_category0
+        )
+        print(self.test_category)
+        print(self.test_category0)
+        print(self.test_category1)
     def test_get(self):
         url = "/categories/"
         response = self.client.get(url)
@@ -18,6 +23,13 @@ class TestCategories(TestCase):
 
     def test_post(self):
         url = "/categories/"
-        data = {"name": "cup", "parent": self.test_category0}
+        data = {"name": "cup", "parent": 1}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, 201)
+
+    # def test_post_past_level_limit(self):
+    #     url = "/categories/"
+    #     data = {"name": "tea", "parent": 3}
+    #     response = self.client.post(url, data)
+    #     self.assertEqual(response.status_code, 400)
+        
--- categories/views.py
+++ categories/views.py
@@ -22,7 +22,7 @@ class CategoryListView(generics.ListCreateAPIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
 
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()

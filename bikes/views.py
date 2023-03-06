3c2cefde69dc28c3c5d23bed75f1f5a3ae25e52a
Kasper Kerajärvi <kasperkerajarvi@gmail.com>
2023-03-06 11:20:55 +0200

    Removed mock data

--- bikes/views.py
+++ bikes/views.py
@@ -3,12 +3,12 @@
 import datetime
 
 from rest_framework import generics
+
 # from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
 from bikes.models import Bike, BikePackage, BikeStock
-from bikes.serializers import (BikePackageSerializer, BikeSerializer,
-                               BikeStockSerializer)
+from bikes.serializers import BikePackageSerializer, BikeSerializer, BikeStockSerializer
 
 
 class BikeStockList(generics.ListAPIView):
@@ -18,9 +18,7 @@ class BikeStockList(generics.ListAPIView):
 
 
 class MainBikeList(generics.ListAPIView):
-
     def list(self, request):
-
         today = datetime.date.today()
         available_from = today + datetime.timedelta(days=7)
         available_to = today + datetime.timedelta(days=183)
@@ -70,64 +68,6 @@ class MainBikeList(generics.ListAPIView):
                     "available_to": available_to,
                 },
                 "bikes": bike_serializer.data,
-                # "db_bikes": bike_serializer.data,
                 "packages": bike_package_serializer.data,
-                # "bikes": [
-                #     {
-                #         "id": 1,
-                #         "name": "Punainen hieno pyörä",
-                #         "description": "Hyväkuntone hieno pyörä punainen suoraa 80-luvulta",
-                #         "max_available": 0,
-                #         "unavailable": {},
-                #         "size": "14″",
-                #         "type": "City",
-                #         "color": "Punainen",
-                #         "brand": "Hieno",
-                #     },
-                #     {
-                #         "id": 2,
-                #         "name": "Vihreä hyvä pyörä",
-                #         "description": "Todella hyvä pyörä",
-                #         "max_available": 17,
-                #         "unavailable": {"15.03.2023": 1, "16.03.2023": 2, "20.03.2023": 1},
-                #         "size": "21″",
-                #         "type": "BMX",
-                #         "color": "Vihreä",
-                #         "brand": "Hyvä",
-                #     },
-                #     {
-                #         "id": 3,
-                #         "name": "Toinen hyvä pyörä",
-                #         "description": "Todella hyvä pyörä myös",
-                #         "max_available": 9,
-                #         "unavailable": {"15.03.2023": 1, "16.03.2023": 2, "20.03.2023": 1},
-                #         "size": "16″",
-                #         "type": "City",
-                #         "color": "Vihreä",
-                #         "brand": "Hyvä",
-                #     },
-                #     {
-                #         "id": 4,
-                #         "name": "Päiväkoti -paketti",
-                #         "description": "16″ pyöriä 7 kpl, 14″ pyöriä 3 kpl, potkupyöriä 10 kpl, pyöräilykypäriä 20 kpl, käsipumppu, jalkapumppu, monitoimityökalu",
-                #         "max_available": 2,
-                #         "unavailable": {"15.03.2023": 1, "16.03.2023": 2, "20.03.2023": 1},
-                #         "size": "14″ & 16″",
-                #         "type": "Paketti",
-                #         "color": "Monia",
-                #         "brand": "Hyvä",
-                #     },
-                #     {
-                #         "id": 5,
-                #         "name": "Koulu -paketti",
-                #         "description": "20″ pyöriä 6 kpl, 24″ pyöriä 6 kpl, pyöräilykypäriä 13 kpl, käsipumppu, jalkapumppu, monitoimityökalu, molempia pyöriä olemassa 7 kpl, mutta tällä määrällä peräkärry on helppo lastata",
-                #         "max_available": 2,
-                #         "unavailable": {"15.03.2023": 1, "16.03.2023": 2, "20.03.2023": 1},
-                #         "size": "20″ & 24″",
-                #         "type": "Paketti",
-                #         "color": "Monia",
-                #         "brand": "Hyvä",
-                #     },
-                # ],
             }
         )

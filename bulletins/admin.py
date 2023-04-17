257e01ec6b1fec6c5adf6793d97e136eb813b23c
Platos-Man <joonasru@tutanota.com>
2023-04-17 09:01:51 +0300

    removed bulletin_subject references

--- bulletins/admin.py
+++ bulletins/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import Bulletin, BulletinSubject
+from .models import Bulletin
 
 # Register your models here.
 
 admin.site.register(Bulletin)
-admin.site.register(BulletinSubject)

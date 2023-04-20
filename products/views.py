ebd9bc4ba95548e810566d2b8014a55fec5e0ab5
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-04-20 08:22:45 +0300

    asd

--- products/views.py
+++ products/views.py
@@ -156,10 +156,10 @@ class ProductListView(generics.ListAPIView):
         ],
         # request=
         #     {"application/json": (inline_serializer("Product amount", fields={"amount": serializers.IntegerField(), "other": ProductSerializer()}))}
-        request=
-            {"application/json": (inline_serializer(fields={"amount": serializers.IntegerField()}, **ProductSerializer))}
         # request=
-        #     {"application/json": ProductCreateSerializer()},
+        #     {"application/json": (inline_serializer(fields={"amount": serializers.IntegerField()}, **ProductSerializer))}
+        request=
+            {"application/json": ProductCreateSerializer()},
     )
 )
 class StorageProductListView(generics.ListCreateAPIView):

144a332c759facc06342e7d0fe038c90e9cae584
Platos-Man <joonasru@tutanota.com>
2023-04-25 12:42:05 +0300

    added comment for example extra_kwargs

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,18 +1,21 @@
 from rest_framework import serializers
 
-from .models import Order, ShoppingCart
 from products.serializers import ProductSerializer
+
+from .models import Order, ShoppingCart
+
 
 class ShoppingCartSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShoppingCart
         fields = "__all__"
 
+
 class OrderSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Order
         fields = "__all__"
+
 
 class ShoppingCartDetailSerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
@@ -21,10 +24,11 @@ class ShoppingCartDetailSerializer(serializers.ModelSerializer):
         model = ShoppingCart
         fields = "__all__"
 
+
 class OrderDetailSerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
         fields = "__all__"
-        
+        # extra_kwargs = {"delivery_address": {"required": True}}

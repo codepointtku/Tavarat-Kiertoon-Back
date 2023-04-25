7d1c8cf1b9c93f9ca945ec909794debfba5516c1
Platos-Man <joonasru@tutanota.com>
2023-04-25 13:31:32 +0300

    Revert "added comment for example extra_kwargs"
    This reverts commit 144a332c759facc06342e7d0fe038c90e9cae584.

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,21 +1,18 @@
 from rest_framework import serializers
 
-from products.serializers import ProductSerializer
-
 from .models import Order, ShoppingCart
-
+from products.serializers import ProductSerializer
 
 class ShoppingCartSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShoppingCart
         fields = "__all__"
 
-
 class OrderSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Order
         fields = "__all__"
-
 
 class ShoppingCartDetailSerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
@@ -24,11 +21,10 @@ class ShoppingCartDetailSerializer(serializers.ModelSerializer):
         model = ShoppingCart
         fields = "__all__"
 
-
 class OrderDetailSerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
         fields = "__all__"
-        # extra_kwargs = {"delivery_address": {"required": True}}
+        

7fa80c349e17de7e15e613a453108ff59880682d
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-04-27 08:49:41 +0300

    removed unused code

--- bikes/views.py
+++ bikes/views.py
@@ -4,10 +4,10 @@ import datetime
 import math
 
 # from rest_framework.permissions import IsAdminUser
-from rest_framework import generics, status, serializers
+from rest_framework import generics, status
 from rest_framework.response import Response
 
-from drf_spectacular.utils import extend_schema_view, extend_schema, inline_serializer
+from drf_spectacular.utils import extend_schema_view, extend_schema
 
 from bikes.models import Bike, BikePackage, BikeRental, BikeStock
 from bikes.serializers import (
@@ -78,41 +78,6 @@ class BikeStockDetailView(generics.RetrieveUpdateDestroyAPIView):
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
 
-# @extend_schema_view(
-#     get=extend_schema(
-#         description="list of bike renal calendars start and end date and all bikes and packages",
-#         request={},
-#         responses={
-#             200: inline_serializer(
-#                 "MainBikelist",
-#                 fields={
-#                     "date_info": inline_serializer(
-#                         "date_info",
-#                         fields={
-#                             "available_from": serializers.DateField(),
-#                             "available_to": serializers.DateField(),
-#                         },
-#                     ),
-#                     "bikes": inline_serializer(
-#                         "bikes",
-#                         fields={
-#                                 "id": serializers.IntegerField(),
-#                                 "name": serializers.CharField(),
-#                                 "max_available": serializers.IntegerField(),
-#                                 "description": serializers.CharField(),
-#                                 "type": serializers.CharField(),
-#                                 "brand": serializers.CharField(),
-#                                 "size": serializers.CharField(),
-#                                 "unavailable": serializers.CharField(),
-#                                 "package_only_count": serializers.IntegerField(),
-#                                 "package_only_unavailable": serializers.CharField(),
-#                         },
-#                     ),
-#                 },
-#             ),
-#         },
-#     ),
-# )
 class MainBikeList(generics.ListAPIView):
     serializer_class = MainBikeListSchemaSerializer
     queryset = Bike.objects.none()

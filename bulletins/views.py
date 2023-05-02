36f248ea0413bcef193e3d0e164a59e89a39ecfd
Platos-Man <joonasru@tutanota.com>
2023-05-02 08:37:11 +0300

    added all fields to required for bulletins

--- bulletins/views.py
+++ bulletins/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Bulletin
-from .serializers import BulletinSerializer
+from .serializers import BulletinResponseSerializer, BulletinSerializer
 
 
 # Create your views here.
@@ -14,7 +15,31 @@ class BulletinListView(ListCreateAPIView):
     ordering_fields = ["id"]
     ordering = ["-id"]
 
+    @extend_schema(responses=BulletinResponseSerializer)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    @extend_schema(responses=BulletinResponseSerializer)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
 
 class BulletinDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Bulletin.objects.all()
     serializer_class = BulletinSerializer
+
+    @extend_schema(responses=BulletinResponseSerializer)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    @extend_schema(responses=BulletinResponseSerializer)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    @extend_schema(methods=["PATCH"], exclude=True)
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    @extend_schema(responses=BulletinResponseSerializer)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)

046b63e6e59650dcfa7f07fc48a6263da3825d10
Platos-Man <joonasru@tutanota.com>
2023-05-02 07:47:26 +0300

    tried to make author required

--- bulletins/models.py
+++ bulletins/models.py
@@ -8,7 +8,9 @@ class Bulletin(models.Model):
     """class for making Bulletin table for database"""
 
     id = models.BigAutoField(primary_key=True)
-    author = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(
+        CustomUser, on_delete=models.SET_NULL, null=True, blank=False
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)

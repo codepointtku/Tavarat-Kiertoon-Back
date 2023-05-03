e9f103b81d17e00a2053b6f433c307b0a677f776
Platos-Man <joonasru@tutanota.com>
2023-05-03 10:05:52 +0300

    Created ModifyProduct database table

--- products/models.py
+++ products/models.py
@@ -1,9 +1,12 @@
 from os.path import basename
 
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
 
 from categories.models import Category
+
+CustomUser = get_user_model()
 
 
 # Create your models here.
@@ -39,21 +42,33 @@ class Storage(models.Model):
         return f"Storage: {self.name}({self.id})"
 
 
+class ModifyProduct(models.Model):
+    """class for tracking when Product was modified and why"""
+
+    class CircumstanceChoices(models.TextChoices):
+        """Choices for the state of the bike."""
+
+        SHOPPINGCART = "SHOPPING CART"
+        ORDER = "ORDER"
+        STORAGE = "STORAGE"
+
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    circumstance = models.CharField(max_length=255, choices=CircumstanceChoices.choices)
+
+
 class Product(models.Model):
     """class for making Product table for database"""
 
     id = models.BigAutoField(primary_key=True)
     available = models.BooleanField(default=False)
     barcode = models.CharField(max_length=255, default="")
-    category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
-    )
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     group_id = models.CharField(max_length=255, default="")
     name = models.CharField(max_length=255)
     price = models.FloatField(default=0.0)
-    storages = models.ForeignKey(
-        Storage, on_delete=models.SET_NULL, null=True
-    )
+    storages = models.ForeignKey(Storage, on_delete=models.SET_NULL, null=True)
     shelf_id = models.IntegerField(blank=True, null=True)
     free_description = models.TextField(default="", blank=True)
     pictures = models.ManyToManyField(Picture, blank=True)
@@ -62,6 +77,7 @@ class Product(models.Model):
     measurements = models.CharField(max_length=50, default="", blank=True)
     color = models.ForeignKey(Color, on_delete=models.SET_NULL, null=True, blank=True)
     weight = models.FloatField(default=0.0)
+    modified = models.ManyToManyField(ModifyProduct, blank=True)
 
     def __str__(self) -> str:
         return f"Product: {self.name}({self.id})"

3663fe6d714284f4186505c79d035b52f50a831f
Platos-Man <joonasru@tutanota.com>
2023-05-05 13:43:10 +0300

    cleaned bulletins

--- bulletins/views.py
+++ bulletins/views.py
@@ -1,4 +1,4 @@
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
@@ -7,6 +7,10 @@ from .serializers import BulletinResponseSerializer, BulletinSerializer
 
 
 # Create your views here.
+@extend_schema_view(
+    get=extend_schema(responses=BulletinResponseSerializer),
+    post=extend_schema(responses=BulletinResponseSerializer),
+)
 class BulletinListView(ListCreateAPIView):
     queryset = Bulletin.objects.all()
     serializer_class = BulletinSerializer
@@ -14,27 +18,12 @@ class BulletinListView(ListCreateAPIView):
     ordering_fields = ["id"]
     ordering = ["-id"]
 
-    @extend_schema(responses=BulletinResponseSerializer)
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    @extend_schema(responses=BulletinResponseSerializer)
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
+@extend_schema_view(
+    get=extend_schema(responses=BulletinResponseSerializer),
+    put=extend_schema(responses=BulletinResponseSerializer),
+    patch=extend_schema(exclude=True),
+)
 class BulletinDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Bulletin.objects.all()
     serializer_class = BulletinSerializer
-
-    @extend_schema(responses=BulletinResponseSerializer)
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    @extend_schema(responses=BulletinResponseSerializer)
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    @extend_schema(methods=["PATCH"], exclude=True)
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)

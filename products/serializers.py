e5a5e5d97601a8864fe9aedb0728f778383140da
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-05-15 11:34:24 +0300

    Added serializers for ProductListView

--- products/serializers.py
+++ products/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Color, Picture, Product, Storage
+from .models import Color, Picture, Product, ProductItem, Storage
 
 
 class PictureSerializer(serializers.ModelSerializer):
@@ -15,10 +15,22 @@ class PictureSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category_name = serializers.ReadOnlyField(source="category.name")
-    color_name = serializers.ReadOnlyField(source="color.name")
-    storage_name = serializers.ReadOnlyField(source="storages.name")
     pictures = PictureSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+class ProductItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= ProductItem
+        fields = "__all__"
+
+
+class ProductCreateSerializer(serializers.ModelSerializer):
+    product_item = ProductItemSerializer()
+    amount = serializers.IntegerField()
 
     class Meta:
         model = Product
@@ -101,21 +113,21 @@ class ProductColorStringSerializer(serializers.ModelSerializer):
         }
 
 
-class ProductCreateSerializer(serializers.ModelSerializer):
-    amount = serializers.IntegerField()
-    pictures = serializers.FileField()
-    color = serializers.IntegerField()
+# class ProductCreateSerializer(serializers.ModelSerializer):
+#     amount = serializers.IntegerField()
+#     pictures = serializers.FileField()
+#     color = serializers.IntegerField()
 
-    class Meta:
-        model = Product
-        fields = "__all__"
-        extra_kwargs = {
-            "available": {"required": True},
-            "barcode": {"required": True},
-            "category": {"required": True},
-            "storages": {"required": True},
-            "color": {"required": True},
-        }
+#     class Meta:
+#         model = Product
+#         fields = "__all__"
+#         extra_kwargs = {
+#             "available": {"required": True},
+#             "barcode": {"required": True},
+#             "category": {"required": True},
+#             "storages": {"required": True},
+#             "color": {"required": True},
+#         }
 
 
 class ProductStorageTransferSerializer(serializers.Serializer):

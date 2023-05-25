f5afbe729c777e1bf13594de2b1d3a8d4f93cc6a
Platos-Man <joonasru@tutanota.com>
2023-05-25 10:46:20 +0300

    removed unused serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from products.serializers import (
-    ProductItemDetailSchemaResponseSerializer,
     ProductItemSchemaResponseSerializer,
     ProductItemSerializer,
 )

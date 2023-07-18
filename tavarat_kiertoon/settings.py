c4a46bf1d6b9f4bcb6683f8f57646b08e4127d14
Metarails <SamiSantamaa87@gmail.com>
2023-07-18 12:58:00 +0300

    removed old line in settings

--- tavarat_kiertoon/settings.py
+++ tavarat_kiertoon/settings.py
@@ -178,7 +178,6 @@ SIMPLE_JWT = {
     "AUTH_COOKIE_PATH": "/",  # The path of the auth cookie.
     "AUTH_COOKIE_SAMESITE": "Lax",  # Whether to set the flag restricting cookie leaks on cross-site requests. This can be 'Lax', 'Strict', or None to disable the flag.
 }
-REFRESH_TOKEN_REMEMBER_ME = config("REFRESH_TOKEN_REMEMBER_ME")
 
 SPECTACULAR_SETTINGS = {
     "ENUM_NAME_OVERRIDES": {

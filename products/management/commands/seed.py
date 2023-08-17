29bb896d523997a83837e2a4ed37cc0d8e33155d
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-08-17 14:49:27 +0300

    basic seed data for trailers

--- products/management/commands/seed.py
+++ products/management/commands/seed.py
@@ -18,6 +18,8 @@ from bikes.models import (
     BikeSize,
     BikeStock,
     BikeType,
+    BikeTrailerModel,
+    BikeTrailer,
 )
 from bulletins.models import Bulletin
 from categories.models import Category
@@ -1005,6 +1007,30 @@ def create_bike_package():
             amount_object.save()
 
 
+def create_bike_trailers():
+    trailer_model = BikeTrailerModel(
+        name="Peräkärry", description="Peräkärry pyöräpakettien kuljettamiseen"
+    )
+    trailer_model.save()
+
+    trailers = [
+        {
+            "register_number": "ASD-123",
+        },
+        {
+            "register_number": "QWE-123",
+        },
+        {
+            "register_number": "ZXC-123",
+        },
+    ]
+    for trailer in trailers:
+        trailer_object = BikeTrailer(
+            register_number=trailer["register_number"], trailer_type=trailer_model
+        )
+        trailer_object.save()
+
+
 def run_seed(self, mode):
     """Seed database based on mode.
 
@@ -1034,3 +1060,4 @@ def run_seed(self, mode):
     create_bikes()
     create_bike_stock()
     create_bike_package()
+    create_bike_trailers()

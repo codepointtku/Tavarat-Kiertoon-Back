1388840cd8a9c9fc7e1b2296067cce65c02503d7
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-08-29 11:39:00 +0300

    Added log entries to clear_shopping_carts function

--- cron.py
+++ cron.py
@@ -1,11 +1,23 @@
-from orders.models import ShoppingCart
 import datetime
+
 from django.utils import timezone
+
+from orders.models import ShoppingCart
+from products.models import ProductItemLogEntry
 
 
 def clear_shopping_carts():
-    queryset = ShoppingCart.objects.exclude(product_items=None).filter(date__lte=timezone.now() - datetime.timedelta(minutes=5))
+    queryset = ShoppingCart.objects.exclude(product_items=None).filter(
+        date__lte=timezone.now() - datetime.timedelta(minutes=5)
+    )
     for cart in queryset:
+        log_entry = ProductItemLogEntry.objects.create(
+            action=ProductItemLogEntry.ActionChoices.CART_TIMEOUT
+        )
+        for product_item in cart.product_items.all():
+            product_item.log_entries.add(log_entry)
+            product_item.available = True
+            product_item.save()
         cart.product_items.clear()
         cart.save()
     return

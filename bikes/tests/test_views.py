b27195354e30d628c245d6243d5a09e786b8d70c
Jminkkinen <121083062+Jminkkinen@users.noreply.github.com>
2023-09-18 15:46:00 +0300

    added test users

--- bikes/tests/test_views.py
+++ bikes/tests/test_views.py
@@ -19,9 +19,63 @@ from bikes.models import (
 from products.models import Color, Storage
 
 
-class TestProducts(TestCase):
+class TestBikes(TestCase):
     @classmethod
-    @override_settings(MEDIA_ROOT=TEST_DIR)
     def setUpTestData(cls):
-        asd = "asd"
+        cls.test_user1 = CustomUser.objects.create_user(
+            first_name="Bike",
+            last_name="Admin",
+            email="bikeadmin@turku.fi",
+            phone_number="0501111111",
+            password="bikeadmin",
+            address="Bikealley 1",
+            zip_code="99999",
+            city="Bikeland",
+            username="bikeadmin@turku.fi",
+        )
+        cls.test_user1.is_active = True
+        cls.test_user1.save()
+
+        cls.test_user2 = CustomUser.objects.create_user(
+            first_name="Biker",
+            last_name="Person",
+            email="bikerperson@turku.fi",
+            phone_number="0502222222",
+            password="bikerperson",
+            address="Bikealley 2",
+            zip_code="99999",
+            city="Bikeland",
+            username="bikerperson@turku.fi",
+        )
+        cls.test_user2.is_active = True
+        cls.test_user2.save()
+
+        cls.test_bikesize = BikeSize.objects.create(
+            name="48″",
+        )
+
+        if Group.objects.filter(name="admin_group").count() == 0:
+            cls.test_group_admin = Group.objects.create(name="admin_group")
+            cls.test_group_admin.user_set.add(cls.test_user1)
+        if Group.objects.filter(name="user_group").count() == 0:
+            cls.test_group_user = Group.objects.create(name="user_group")
+            cls.test_group_user.user_set.add(cls.test_user1)
+            cls.test_group_user.user_set.add(cls.test_user2)
+        if Group.objects.filter(name="storage_group").count() == 0:
+            cls.test_group_storage = Group.objects.create(name="storage_group")
+            cls.test_group_storage.user_set.add(cls.test_user1)
+        if Group.objects.filter(name="bicycle_group").count() == 0:
+            cls.test_group_bicycle = Group.objects.create(name="bicycle_group")
+            cls.test_group_bicycle.user_set.add(cls.test_user2)
+            cls.test_group_bicycle.user_set.add(cls.test_user1)
+
+    def login_test_user(self):
+        url = "/users/login/"
+        data = {
+            "username": "bikeadmin@turku.fi",
+            "password": "bikeadmin",
+        }
+        self.client.post(url, data, content_type="application/json")
+        user = CustomUser.objects.get(username="bikeadmin@turku.fi")
+        return user
 

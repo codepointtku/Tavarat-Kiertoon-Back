215711f3e83085cebca6f3b4faf8323db44535d4
Platos-Man <joonasru@tutanota.com>
2023-10-03 13:14:49 +0300

    product creation to database.py

--- products/management/commands/database.py
+++ products/management/commands/database.py
@@ -1,3 +1,4 @@
+import json
 from csv import reader
 from typing import Any
 
@@ -6,7 +7,7 @@ from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand
 
 from categories.models import Category
-from products.models import Color, Product, ProductItem, Storage
+from products.models import Color, Picture, Product, ProductItem, Storage
 
 CustomUser = get_user_model()
 
@@ -28,6 +29,8 @@ def clear_data():
     Color.objects.all().delete()
     Storage.objects.all().delete()
     Category.objects.all().delete()
+    Product.objects.all().delete()
+    Picture.objects.all().delete()
 
 
 def super_user():
@@ -49,21 +52,84 @@ def colors():
 
 
 def storages():
-    file = reader(open("tk-db/storages.csv", encoding="utf8"))
-    next(file)
-    for row in file:
-        Storage.objects.create(
-            name=row[1], address=row[2], zip_code=row[3], city=row[4]
-        )
+    Storage.objects.create(
+        name="Iso-Heikkilän Varasto",
+        address="Iso-Heikkiläntie 6",
+        zip_code="20200",
+        city="Turku",
+    )
 
 
 def products():
-    file = reader(open("tk-db/products.csv", encoding="utf8"))
+    file = reader(open("tk-db/products_full.csv", encoding="utf8"))
     header = next(file)
-    products = {}
+    products = []
     for row in file:
-        products[int(row[0])] = {header[1]: row[1], header[2]: row[2]}
-    print(products)
+        products.append(
+            {
+                header[0]: row[0],
+                header[1]: row[1],
+                header[2]: row[2],
+                header[3]: row[3],
+                header[4]: row[4],
+                header[5]: row[5],
+                header[6]: row[6],
+                header[7]: row[7],
+                header[8]: row[8],
+                header[9]: row[9],
+                header[10]: row[10],
+                header[11]: row[11],
+            }
+        )
+    for product in products:
+        pictures = set(json.loads(product["file"]))
+        for picture in pictures:
+            if picture != None:
+                Picture.objects.create(picture_address=picture)
+
+        picture_objects = [
+            Picture.objects.get(picture_address=picture_address).id
+            for picture_address in pictures
+            if picture_address != None
+        ]
+
+        color_objects = [
+            Color.objects.get(name=color_name).id
+            for color_name in json.loads(product["color"])
+            if color_name != None
+        ]
+
+        free_description = product["free_description"]
+        if free_description == "NULL":
+            free_description = ""
+        try:
+            weight = float(product["weight"])
+        except ValueError:
+            weight = 0.0
+
+        product_object = Product.objects.create(
+            category=Category.objects.get(name="Muut Tuolit"),
+            name=product["name"],
+            price=float(product["price"]),
+            free_description=free_description,
+            # pictures
+            measurements=product["measurements"],
+            # colors
+            weight=weight,
+        )
+
+        for _ in range(int(float(product["amount"]))):
+            ProductItem.objects.create(
+                product=product_object,
+                available=True,
+                storage=Storage.objects.get(name="Iso-Heikkilän Varasto"),
+                barcode=product["barcode"],
+            )
+
+        if color_objects:
+            product_object.colors.add(*color_objects)
+        if picture_objects:
+            product_object.pictures.add(*picture_objects)
 
 
 def categories():

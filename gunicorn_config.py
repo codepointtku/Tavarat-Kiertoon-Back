799f3476e549ced41ffad2cf16cb07c274064e53
codepointpetteri <petteri.anttila@turku.fi>
2024-01-15 10:03:02 +0200

    added production and dev docker compose file

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,11 +5,7 @@ key = os.getenv("SSL_KEY", "./ssl/*.key")
 cert = os.getenv("SSL_CRT", "./ssl/*.crt")
 
 certfile = glob.glob(cert)[0]
-
 keyfile = glob.glob(key)[0]
-
-print(certfile)
-print(keyfile)
 
 bind = os.getenv("WEB_BIND", "0.0.0.0:8000")
 reload = os.getenv("WEB_RELOAD", "false")

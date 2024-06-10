2f106cd92d5a2de28d06cbf6ebfd27a756e47306
codepointpetteri <petteri.anttila@turku.fi>
2024-06-10 09:48:16 +0300

    gunicorn logging to files

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,10 +2,14 @@ import glob
 import os
 
 key = os.getenv("SSL_KEY", "./ssl/*.key")
-cert = os.getenv("SSL_CRT", "./ssl/*.crt")
+cert = os.getenv("SSL_CERT", "./ssl/*.crt")
 
 certfile = glob.glob(cert)[0]
 keyfile = glob.glob(key)[0]
 
 bind = os.getenv("WEB_BIND", "0.0.0.0:8000")
 reload = os.getenv("WEB_RELOAD", "false")
+
+accesslog = "/logs/tk-access.log"
+errorlog = "/logs/tk-error.log"
+loglevel = "warning"

79f51732b999f67ecafc1f7555f17727217d0e9e
codepointpetteri <102511124+codepointpetteri@users.noreply.github.com>
2026-09-14 13:49:04 +0300

    send email at 7.00

--- tavarat_kiertoon/settings.py
+++ tavarat_kiertoon/settings.py
@@ -259,8 +259,16 @@ URL_FRONT = config("URL_FRONT")
 
 CRONJOBS = [
     ("0 * * * *", "cron.clear_shopping_carts", ">> /usr/src/app/file.log"),
-    ("0 * * * *", "cron.notify_user_of_bike_rental_start", ">> /usr/src/app/file.log"),
-    ("0 * * * *", "cron.notify_user_of_bike_rental_end", ">> /usr/src/app/file.log"),
+    (
+        "7 * * * *",
+        "cron.notify_user_of_bike_rental_start",
+        ">> /usr/src/app/file.log 2>&1",
+    ),
+    (
+        "7 * * * *",
+        "cron.notify_user_of_bike_rental_end",
+        ">> /usr/src/app/file.log 2>&1",
+    ),
 ]
 
 
